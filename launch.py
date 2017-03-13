c363ab8e319d9557bc2b3261af4ae651ef91e99c
HongXiang <hx.hongxiang@gmail.com>
2017-03-13 09:03:50 +0800

    new commit

--- launch.py
+++ launch.py
@@ -1,6 +1,10 @@
 """ lauch server """
-
+import sys
 from hqlf import app
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 5000, debug=True)
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 5000
+    app.run(host = "0.0.0.0", port=port, debug=True)
