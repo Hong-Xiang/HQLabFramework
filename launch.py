c4d538db34ba754489667e7799fc80ada5415841
HongXiang <hx.hongxiang@gmail.com>
2017-08-05 11:00:30 +0800

    RESTful init

--- launch.py
+++ launch.py
@@ -1,10 +1,14 @@
 """ lauch server """
 import sys
-from hqlf import app
+# from hqlf import app
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        port = int(sys.argv[1])
-    else:
-        port = 5000
-    app.run(host = "0.0.0.0", port=port, debug=True)
+# if __name__ == '__main__':
+#     if len(sys.argv) > 1:
+#         port = int(sys.argv[1])
+#     else:
+#         port = 5000
+#     app.run(host = "0.0.0.0", port=port, debug=True)
+
+from hqlf.server import server
+
+server.run(debug=True)
